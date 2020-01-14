a93343e1c79a084522b3d84d4bce1a64cbd17bfe
Yipeng Sun <syp@umd.edu>
2020-01-13 22:32:35 -0500

    Cleanups.

--- configuration.nix
+++ configuration.nix
@@ -77,7 +77,10 @@
   # cursor) then try increasing this to 256M.  On a Raspberry Pi 4 with 4 GB,
   # you should either disable this parameter or increase to at least 64M if you
   # want the USB ports to work.
-  boot.kernelParams = ["cma=32M,dtoverlay=w1-gpio,pullup=1"];
+  boot.kernelParams = [
+    "cma=32M"
+    "dtoverlay=w1-gpio,pullup=1"  # For thermistors 1-wire setup
+  ];
 
   # Other rpi tunings
   boot.loader.raspberryPi.enable = true;
