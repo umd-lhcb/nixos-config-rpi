b5547f6c65ab9b46158f09f91cd747fb07da50db
Yipeng Sun <syp@umd.edu>
2020-01-16 05:34:10 -0500

    Fix the udev package.

--- overlays/burnin/usb-relay-udev-rules/default.nix
+++ overlays/burnin/usb-relay-udev-rules/default.nix
@@ -1,12 +1,15 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv }:
 
 stdenv.mkDerivation rec {
   pname = "usb-relay-udev-rules";
   version = "20200116";
 
-  src = ./50-usb-relay-dct-tech.rules
+  src = ./50-usb-relay-dct-tech.rules;
+
+  # Remove all but installPhase
+  phases = [ "installPhase" ];
 
   installPhase = ''
-    install -D 50-usb-relay-dct-tech.rules $out/lib/udev/rules.d/50-usb-relay-dct-tech.rules
+    install -D $src $out/lib/udev/rules.d/50-usb-relay-dct-tech.rules
   '';
 }
