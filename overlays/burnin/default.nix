4823b2c365d8a1ffb03b7a1e1171f9aa2ab85ad1
Yipeng Sun <syp@umd.edu>
2020-01-16 03:11:26 -0500

    Extending rpi kernel.

--- overlays/burnin/default.nix
+++ overlays/burnin/default.nix
@@ -3,11 +3,16 @@ self: super:
 let
   pythonOverrides = {
     packageOverrides = self: super: {
-      RPi.GPIO = super.callPackage ./RPi.GPIO { };
+      RPi.GPIO = super.callPackage ./RPi.GPIO {};
     };
   };
 in
 
 {
   python3 = super.python3.override pythonOverrides;
+
+  linuxPackages_rpi4 = super.linuxPackages_rpi4.extend(
+    lpself: lpsuper: {
+      w1-gpio-cl = lpself.callPackage ./w1-gpio-cl {};
+  });
 }
