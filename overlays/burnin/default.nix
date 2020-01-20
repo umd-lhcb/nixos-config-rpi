532883ad368412c71f715d4aa7118116e4410882
Yipeng Sun <syp@umd.edu>
2020-01-20 06:12:48 -0500

    Edit typo.

--- overlays/burnin/default.nix
+++ overlays/burnin/default.nix
@@ -5,7 +5,7 @@ let
     packageOverrides = self: super: {
       RPi_GPIO = super.callPackage ./RPi.GPIO {};
       rpi_burnin = super.callPackage ./rpi.burnin {};
-      rainbow_logging_handler = super.callPackage ./rainbow_logging_handle {};
+      rainbow_logging_handler = super.callPackage ./rainbow_logging_handler {};
     };
   };
 
